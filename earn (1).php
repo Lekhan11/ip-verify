3c51f330b9e323b41762ab7d7cf8a4362e6ab299
Lekhan11 <89090159+Lekhan11@users.noreply.github.com>
2023-04-12 19:40:38 +0530

    Add files via upload

---
+++ earn (1).php
@@ -0,0 +1,64 @@
+
+
+<script> if(performance.navigation.type == 2){ location.reload(true); } </script> <!DOCTYPE html> <html lang="en"> <!-- Required meta tags --> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <meta name="description" content="Select Offer and Enter Paytm number and complete simple task to get cashback "> <link rel="shortcut icon" href="https://www.lootchamp.in/uploads/images/favicon.png" type="image/x-icon"> <link href="https://www.lootchamp.in/assets/css/app.css" rel="stylesheet"> <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet"> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <title> EarnCashBacks </title> </head> <body> <style> body, .form-control, .promo-content { background-color: #EAECF5; } .home-wrapper, .promo-offer-block, .form-control, .promo-content { color: #4f5a80; } .hp-social-title, .promo-head-title, .frm-disc, .form-control:focus, .frm-label { color: #314483; } .label-disc { color: #8d9ab1; } .promo-offer-block, .home-wrapper { background-image: linear-gradient(135deg, #fff, #eef6fa); box-shadow: 0 15px 50px 0 rgb(85, 95, 109, .1); } .primary-btn { /* border-color: #4c60a2; */ display: inline-block; background-color: #FFC500; color: #000000; border: 1px solid transparent; /* background-image: linear-gradient(135deg, #638fba, #46569e); */ /* box-shadow: 2px 6px 15px rgba(88, 114, 181, .35); */ padding: 8px 20px; transition: .1s ease-in-out; transition-property: color, background-color, background-position, border-color, box-shadow; border-radius: 500px; font-family: Montserrat; font-weight: 900; border: 1px solid black; font-size: 13px; text-transform: uppercase; } .select { /* border-color: #4c60a2; */ display: inline-block; background-color: #FF9F93; color: black; border: 1px solid green; /* background-image: linear-gradient(135deg, #638fba, #46569e); */ /* box-shadow: 2px 6px 15px rgba(88, 114, 181, .35); */ padding: 8px 20px; transition: .1s ease-in-out; transition-property: color, background-color, background-position, border-color, box-shadow; border-radius: 10px; font-family: Montserrat; text-align: center; font-weight: 700; font-size: 13px; text-transform: uppercase; } .btn-left-icon { max-height: 30px; margin-right: 10px; } .primary-btn:hover { color: #fff; text-decoration: none; } button:focus { outline: 0; } </style> <section class="promo-main-section"> <div class="promo-banner" style="background-image: url('https://www.lootchamp.in/1643783725-5065951.jpeg');"> <div class="promo-banner-content text-white position-relative text-center"> <div class="container"> <div class="pro-banner-head"style="height: 130px;"> <h1> <?php echo $camp; ?> </h1> </div> </div> </div> </div> <div class="container"> <div class="row justify-content-center"> <div class="col-lg-8 col-md-9"> <div class="promo-offer-block rounded bg-white mb-4"> <div class="promo-offer-head border-bottom"> <div class="p-3"> <h2 class="promo-head-title">Claim Fast Our Lifafa</h2> </div> </div> <div class="promo-offer-content"> <div class="promo-frm p-3"> <div class="form-group row"> 	 <label for="inputEmail3" class="col-lg-5 col-form-label"> <span class="d-block frm-label mb-2"> After Redirect By Site Start Bot And Join all Channel </span> <span class="label-disc">Cashback Will be Transfer to Your Paytm Number Instantly.</span> </label> <div class="col-lg-7"> 	
+<?php
+
+error_reporting(0);
+
+if(!isset($_GET["Ssubmit"])){
+	
+
+$id=$_GET["id"];
+$ip=$_SERVER["REMOTE_ADDR"];
+if($id==""){
+
+	echo"<meta http-equiv='refresh' content=2;url=https://Telegram
+	Dog/Cashback_Earner>";
+		
+	echo"Tg Id Required";
+}elseif(file_exists("".$ip.".txt")){
+	echo"<meta http-equiv='refresh' content=2;url=https://telegram.dog/cashback_earner>";
+	file_put_contents("".$ip.".txt",$id);
+	file_put_contents("".$id.".txt",$ip);		
+	echo"Fake refer Not Allowed 😡";
+
+	file_put_contents("".$ip.".txt",$id);
+	file_put_contents("".$id.".txt",$ip);
+	
+}else{
+	
+
+error_reporting(0);
+
+if(!isset($_POST['submit'])){
+echo '<div class="form">
+<form action="" method="POST" >
+    
+<div>
+<form action="" method="GET" >
+  <br><br>      
+<center><input type="submit" id="submit" name="submit" value="CLAIM" class="primary-btn px-5 w-full">
+</form></div></center>';
+  }
+
+
+
+       if(isset($_POST['submit'])){
+       	$paytm2=$_POST['paytm2'];
+       	
+       	echo "Verification Successful...!!!!!";
+
+ echo"<meta http-equiv='refresh' content='0;url=https://telegram.dog/LifafaGiveaway_Tbot?start=OOO$id'>";
+       	
+	file_put_contents("".$ip.".txt",$id);
+	file_put_contents("".$id.".txt",$ip);	
+	
+	file_put_contents("".$ip.".txt",$id);
+	file_put_contents("".$id.".txt",$ip);
+	
+}
+}
+}
+
+?> 
+	  </script> 		 <body> <html>
